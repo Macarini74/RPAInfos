ea6516bb4f9f38ad8dc4916fc0e70fb5e399c178
Iago Macarini <iagoamcarini@alunos.utfpr.edu.br>
2024-09-18 10:45:48 -0300

    ...

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from service import MyService
-from infoMaq import DataCollector
 import win32serviceutil
+import logging
+
+# Configuração do logging
+logging.basicConfig(filename='service_log.txt', level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+logging.debug("Iniciando o script main.py")
 
 if __name__ == '__main__':
-
-    data_collect = DataCollector()
-    data_collect.initCollect()
-    #win32serviceutil.HandleCommandLine(MyService)
+    try:
+        logging.debug("Tentando instalar o servico.")
+        win32serviceutil.HandleCommandLine(MyService)
+    except Exception as e:
+        logging.error(f'Erro ao instalar o serviço: {e}')
