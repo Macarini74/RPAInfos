03451ca0ddf0402106ee91ae5ca1955817d494f8
Iago Macarini <80332375+Macarini74@users.noreply.github.com>
2024-09-23 10:24:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from service import MyService
 if __name__ == '__main__':
     myservice = MyService()
     myservice.initJob()
+    
+# Criar Serviço: sc create NomeDoServiço binPath= "Caminho do Arquivo .exe"
+# Config: sc config NomeDoServiço start= auto
+# Iniciar: sc start NomeDoServiço
